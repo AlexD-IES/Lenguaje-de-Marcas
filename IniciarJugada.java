75aa9c22c7a663c0f23941b60cbd30d4c7c99d78
AlexD-IES <78846942+AlexD-IES@users.noreply.github.com>
2021-03-01 13:35:29 +0100

    Add files via upload

---
+++ IniciarJugada.java
@@ -0,0 +1,18 @@
+package tresEnRaya;
+
+public class IniciarJugada {
+	
+	int tamano=3;
+	int matriz[][] = new int[tamano][tamano];
+	
+	
+	public static int [] iniciarJugada (int[]matriz) {
+		for(int i = 0;i<matriz.length;i++)
+				{
+					for (int j = 0; j < matriz.length; j++) {
+
+					}
+				}
+				
+return matriz;
+}}
